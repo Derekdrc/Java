f2ee507c1729ad4d4efee6d8a869de0d85e523ba
Derekdrc <33431199+Derekdrc@users.noreply.github.com>
2023-11-08 12:25:03 -0500

    Added ability to add new products, fixed bug that would not display new catalog items

--- Assignment3/src/Product.java
+++ Assignment3/src/Product.java
@@ -72,9 +72,7 @@ public class Product {
         this.amountPurchased = amountPurchased;
     }
 
-    public void setDiscount(double discount) {
-        this.discount = discount;
-    }
+    public void setDiscount(double discount) { this.discount = discount; }
 
     public static void main(String[] args) {
         //create arrayList to hold all product objects
@@ -94,8 +92,10 @@ public class Product {
 
         //checks for passcode even if it is not the only argument string provided
         for(int i = 0; i < args.length; i++){
+            //check args array for run time given 'password'
             if(args[i].equals("MCS3603")){
                while(true) {
+                   //Loop through to let admin create as many new products as they would like
                    productCatalog.add(adminConsole(rd, productCatalog));
                    System.out.println("Press '1' if you would like to add another product, press '2' if you would like to exit the admin console: ");
                    int choice = rd.nextInt();
@@ -104,8 +104,12 @@ public class Product {
                        choice = rd.nextInt();
                    }
                    if (choice == 2){
+                       //Clear buffer
+                       rd.nextLine();
                        break;
                    }
+                   //Clear buffer
+                   rd.nextLine();
                }
             }
         }
@@ -119,11 +123,9 @@ public class Product {
             }
         }
 
-        //Clear buffer
-        rd.nextLine();
 
+        //Get users name
         System.out.println("Please enter customer name: ");
-
         String customerName = rd.nextLine();
 
 
@@ -139,28 +141,28 @@ public class Product {
         printCatalog(customerName, productCatalog);
 
         //Get amount purchased of each item from user by calling function
-        for(int i = 0; i<4; i++){
+        for(int i = 0; i< productCatalog.size(); i++){
             productCatalog.get(i).setAmountPurchased(getNumberPurchased(rd, productCatalog, i));
         }
 
+        //Set the discount according to each items quantity purchased
         for (int i = 0; i<productCatalog.size(); i++){
             if(productCatalog.get(i).getAmountPurchased()>=10){
                 productCatalog.get(i).setDiscount((productCatalog.get(i).getProductPrice() * productCatalog.get(i).getAmountPurchased() * .1));
             }
         }
 
-
         //Validating supplied values
         for(int i = 0; i< productCatalog.size(); i++){
             if(productCatalog.get(i).getAmountPurchased()<0){
-                System.out.println("Amount purchased invalid. Please try again with valid codes.");
+                System.out.println("Amount purchased invalid. Please try again with valid amounts.");
                 return;
             }
         }
 
         //Calculate subtotal, discounts and tax
         double subTotal = 0;
-        for(int i = 0; i<4; i++) {
+        for(int i = 0; i< productCatalog.size(); i++) {
             if(productCatalog.get(i).getAmountPurchased() >=10){
                 subTotal += (productCatalog.get(i).getProductPrice() * productCatalog.get(i).getAmountPurchased()) * .9;
             }else{
@@ -168,6 +170,7 @@ public class Product {
             }
         }
 
+        //Calculate the pay in full discount
         System.out.println("Would you like to pay in full, or pay in installments? Press '1' to pay in full now and receive a 5% discount, press '2' for installments.");
         int installChoice = rd.nextInt();
         while(installChoice!=1 && installChoice!=2){
@@ -179,6 +182,7 @@ public class Product {
             payFullDiscount = subTotal*.05;
         }
 
+        //Calculate tax and total
         double tax = (subTotal-payFullDiscount) * .06;
         double total = subTotal-payFullDiscount + tax;
 
@@ -187,57 +191,86 @@ public class Product {
 
         //Close scanner object
         rd.close();
+
     }
 
-    public static Product adminConsole(Scanner rd, ArrayList<Product> cata) {
+    public static Product adminConsole(Scanner rd, ArrayList<Product> catalog) {
+        //This function acts as the admin ability to create a new product and add it to catalog when password is supplied
+        System.out.println("Welcome to the admin console.\n");
         boolean more = true;
             String temp = " ";
             int tempCode = 0;
             String tempName = " ";
             String tempDescr = " ";
             double tempPrice = 0.0;
-            //Do/while checks that user inputted types are correct
+            //Do/while loops until inputs are valid
+            //Item code
             do {
                 try {
                     System.out.println("Please enter a valid item code for the new product: ");
                     temp = rd.nextLine();
                     tempCode = Integer.parseInt(temp);
                     //Validate that product code is unique
-                    for(int i = 0; i< cata.size(); i++){
-                        while(tempCode == cata.get(i).getProductCode()){
+                    for(int i = 0; i< catalog.size(); i++){
+                        while(tempCode == catalog.get(i).getProductCode()){
                             System.out.println("Product code already in use. Try again. ");
                             temp = rd.nextLine();
                             tempCode = Integer.parseInt(temp);
                         }
                     }
+                    //If everything checks out, break out of do while loop to continue
                     break;
                 } catch (Exception e) {
-                    System.out.println("Invalid input. New product will not be added");
+                    //Catch invalid type and make user restart
+                    System.out.println("Invalid input.");
                 }
             }while(true);
-
+            //Item Name
             do {
                 try {
                     System.out.println("Please enter an item name for the new product: ");
+                    boolean nonNumeric = true;
                     temp = rd.nextLine();
-                    tempName = (temp);
-                    break;
+                    char[] chars = temp.toCharArray();
+                    for(int i = 0; i<chars.length; i++){
+                        if(Character.isDigit(chars[i])){
+                            nonNumeric = false;
+                        }
+                    }
+                    if(nonNumeric){
+                        tempName = (temp);
+                        break;
+                    }else{
+                        System.out.println("Invalid input type.");
+                    }
                 } catch (Exception e) {
-                    System.out.println("Invalid input. New product will not be added");
+                    System.out.println("Invalid input.");
                 }
             }while(true);
-
+            //Item Description
             do {
                 try {
                     System.out.println("Please enter an item description for the new product: ");
+                    boolean nonNumeric = true;
                     temp = rd.nextLine();
-                    tempDescr = (temp);
-                    break;
+                    char[] chars = temp.toCharArray();
+                    for(int i = 0; i< chars.length; i++){
+                        if(Character.isDigit(chars[i])){
+                            nonNumeric = false;
+                        }
+                    }
+                    if(nonNumeric){
+                        tempDescr = (temp);
+                        break;
+                    }else{
+                        System.out.println("Invalid input. ");
+                    }
+
                 } catch (Exception e) {
-                    System.out.println("Invalid input. New product will not be added");
+                    System.out.println("Invalid input.");
                 }
             }while (true);
-
+            //Item price
             do{
                 try {
                     System.out.println("Please enter a valid item price for the new product: ");
@@ -245,26 +278,11 @@ public class Product {
                     tempPrice = Double.parseDouble(temp);
                     break;
                 } catch (Exception e) {
-                    System.out.println("Invalid input. New product will not be added");
+                    System.out.println("Invalid input.");
                 }
 
             } while (true);
-
-
-//            //Create new object and add it to catalog
-//            cata.add(new Product(tempCode, tempName, tempDescr, tempPrice));
-//
-//            System.out.println("Press '1' to enter another product, press '2' to exit admin mode. ");
-//            int choice = rd.nextInt();
-//            while(choice != 1 && choice!=2){
-//                System.out.println("Invalid input. Press '1' to enter another product, press '2' to exit admin mode");
-//                choice = rd.nextInt();
-//            }
-//            if (choice == 1){
-//                more = true;
-//            }else{
-//                more = false;
-//            }
+        // Once everything is validated, create new instance of Product object and return it to be added into Catalog arraylist
         return new Product(tempCode, tempName, tempDescr, tempPrice);
     }
 
@@ -275,14 +293,14 @@ public class Product {
         System.out.print("Product Code\t Product name\t Product Description \t\tPrice");
         System.out.println();
         System.out.println("-----------------------------------------------------------------------------------------------------");
-        for(int i = 0; i<4; i++){
+        for(int i = 0; i< catalog.size(); i++){
             System.out.printf("%-17d%-16s%-29s%-29s\n",catalog.get(i).getProductCode(), catalog.get(i).getProductName(), catalog.get(i).getProductDescription(), catalog.get(i).getProductPrice());
         }
     }
 
-    public static int getNumberPurchased(Scanner rd, ArrayList<Product> cata, int currentItem){
+    public static int getNumberPurchased(Scanner rd, ArrayList<Product> catalog, int currentItem){
         //This function gets and verifies input from the user to get how many of a product they are purchasing
-        System.out.println("How many " + cata.get(currentItem).getProductName() + " would you like to purchase?");
+        System.out.println("How many " + catalog.get(currentItem).getProductName() + " would you like to purchase?");
         int purchase = rd.nextInt();
         while (purchase <0){
             System.out.println("Invalid amount. Please enter how a valid number to purchase");
@@ -291,15 +309,16 @@ public class Product {
         return purchase;
     }
 
-    public static void printFinal(String customerName, ArrayList<Product> cata, double subTotal, double payFullDiscount, double tax, double total){
+    public static void printFinal(String customerName, ArrayList<Product> catalog, double subTotal, double payFullDiscount, double tax, double total){
         //This function handles printing out and formatting the final receipt
         System.out.println("\n\n\nCustomer: "+ customerName);
         System.out.println("Itemized Receipt: ");
-        System.out.print("Product Code\t Product name\t Product Description \t\t Amount Purchased \t\t Price Per Item \t\t Item Discounts \t\tPrice");
+        String [] labels = {"Product Code", "Product Name", "Product Description", "Amount Purchased", "Price Per Item", "Item Discounts", "Price"};
+        System.out.printf("%-17s%-16s%-29s%-29s%-25s%-18s%-17s", labels[0], labels[1], labels[2], labels[3], labels[4], labels[5], labels[6]);
         System.out.println();
         System.out.println("--------------------------------------------------------------------------------------------------------------------------------------------------------");
-        for(int i = 0; i<4; i++){
-            System.out.printf("%-17d%-16s%-29s%-29s%.2f%26.2f%17.2f\n",cata.get(i).getProductCode(), cata.get(i).getProductName(), cata.get(i).getProductDescription(), cata.get(i).getAmountPurchased(), cata.get(i).getProductPrice(), (-1*cata.get(i).getDiscount()), (cata.get(i).getProductPrice() * cata.get(i).getAmountPurchased()) - cata.get(i).getDiscount());
+        for(int i = 0; i< catalog.size(); i++){
+            System.out.printf("%-17d%-16s%-29s%-29s%.2f%26.2f%17.2f\n",catalog.get(i).getProductCode(), catalog.get(i).getProductName(), catalog.get(i).getProductDescription(), catalog.get(i).getAmountPurchased(), catalog.get(i).getProductPrice(), (-1*catalog.get(i).getDiscount()), (catalog.get(i).getProductPrice() * catalog.get(i).getAmountPurchased()) - catalog.get(i).getDiscount());
         }
         System.out.printf("\n%129s%10.2f", "Subtotal: ", subTotal);
         System.out.printf("\n%129s%10.2f", "Pay in Full Discount: ",(-1*payFullDiscount));
